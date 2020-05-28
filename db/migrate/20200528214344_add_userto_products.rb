fa760917aa90ebacff38b601a5bae822724bcbaa
vzucher <vzucher@gmail.com>
2020-05-28 18:51:45 -0300

    ran migration AddUserToProduct , generate #1 product seed , added foreign_keys correlations in models, added current_user do ProductsController

---
+++ db/migrate/20200528214344_add_userto_products.rb
@@ -0,0 +1,5 @@
+class AddUsertoProducts < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :products, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_224912) do
+ActiveRecord::Schema.define(version: 2020_05_28_214344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,8 +55,10 @@ ActiveRecord::Schema.define(version: 2020_05_21_224912) do
     t.bigint "personality_type_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["personality_type_id"], name: "index_products_on_personality_type_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "survey_questions", force: :cascade do |t|
@@ -89,6 +91,7 @@ ActiveRecord::Schema.define(version: 2020_05_21_224912) do
   add_foreign_key "orders", "users"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "personality_types"
+  add_foreign_key "products", "users"
   add_foreign_key "survey_questions", "personality_types"
   add_foreign_key "users", "personality_types"
 end
