0ff32ab9e9b9af64db266869f8b7ce19c885deff
vzucher <vzucher@gmail.com>
2020-05-28 21:31:07 -0300

    generate Order and OrderItem when add Product to cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,29 @@
 class OrderItemsController < ApplicationController
+  before_action :set_product, only: [ :create ]
+  def create
+    if current_user.orders.present?
+      @order = current_user.orders.last
+    else
+      @order = current_user.orders.new
+    end
+    @order.save
+    @order_item = @order.order_items.new #(order_item_params)
+    @order_item.product = @product
+    @order_item.save
+  end
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:product_id)
+  end
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
 end
+
+# 1 - Checar se o user ja tem algum Order
+# 2 - if true, pegar current_order e add newProduct
+# 3 - if false, create Order.new and add newProduct
+# 4 -
