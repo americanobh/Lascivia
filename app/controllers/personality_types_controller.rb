c54a868bbea82a046d1e092e57a2b2da3bf5737d
Felipe Americano <felip.americano@gmail.com>
2020-05-30 13:19:55 -0300

    to instalando vários scss e definindo a página de resultados do teste

--- app/controllers/personality_types_controller.rb
+++ app/controllers/personality_types_controller.rb
@@ -1,2 +1,22 @@
 class PersonalityTypesController < ApplicationController
+
+    before_action :find_personality, only: %i[show]
+
+  def index
+    @personality = PersonalityType.all
+  end
+
+
+  def show; end
+
+
+  private
+
+  def personality_params
+    params.require(:personality_type).permit(:name, :description)
+  end
+
+  def find_personality
+    @personality = PersonalityType.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.create(name: "Bullets")
-Category.create(name: "Rabbits")
+Category.create([
+  { name: 'Bullets' },
+  { name: 'Rabbits' },
+  { name: 'Clits' },
+  { name: 'Finger Vibrators' },
+  { name: 'Plug Anal' },
+  { name: 'Velas' },
+  { name: 'Lubrificantes' }]
+)
 
-PersonalityType.create(name: "Pervert", description: "Very Perv")
+
+
+PersonalityType.create(name: "Valente", description: "As valentes vivem num mundo colorido e sensual, inspirado através das conexões com pessoas e ideias. As personalidades Valentes têm alegria em reinterpretar essas conexões, reinventando e experimentando com si próprias e novas perspectivas. Nenhum outro tipo de personalidade explora e experimenta dessa maneira. Isso cria uma sensação de espontaneidade, fazendo os Aventureiros parecerem imprevisíveis, mesmo com seus amigos e entes queridos.")
 
 Product.create(name: "Anal Plug", price: 55.5, description: "Its very fun !", amount: 1, category_id: 2, personality_type_id: 1)
