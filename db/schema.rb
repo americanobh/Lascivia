af06d2ddef8ab7e338fd68b641a398dfd8338deb
Felipe Americano <felip.americano@gmail.com>
2020-05-30 15:25:37 -0300

    seed feita, mas dando erro porque está me obrigando a ter um user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_214344) do
+ActiveRecord::Schema.define(version: 2020_05_30_174640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_214344) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.string "photos"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["personality_type_id"], name: "index_products_on_personality_type_id"
     t.index ["user_id"], name: "index_products_on_user_id"
