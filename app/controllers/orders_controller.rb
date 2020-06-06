c4ae75250c74047df7ce13273707af1113039013
vzucher <vzucher@gmail.com>
2020-06-06 17:08:21 -0300

    modal js fetch between rails controller and js files as json

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+    protect_from_forgery except: :index
 
   def index
-    @orders = Order.all
+    @orders =  @current_order.order_items
+    respond_to do |format|
+      format.js { render json: @orders }
+    end
   end
 
   def show
--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,20 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+const modal = document.getElementById('modal-btn');
+const something = document.getElementById('');
+
+modal.addEventListener('click', (event) => {
+  console.log("It works!");
+  something.innerHTML = '';
+  fetch('/orders')
+    .then(response => response.json())
+    .then((order_items) => {
+      console.log(order_items);
+      order_items.forEach((order_item) => {
+        const product = `<li>${order_item}</li>`;
+        something.insertAdjacentHTML("afterbegin", product);
+      });
+    });
+})
