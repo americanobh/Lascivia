5f9130d9ae66d1204561ba2544664dd767561fa3
vzucher <vzucher@gmail.com>
2020-06-09 20:48:55 -0300

    ajax card modal finnished

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,8 +3,13 @@ class OrdersController < ApplicationController
 
   def index
     @orders =  @current_order.order_items
+    @total_sum = 0
+    @orders.each do |order_item|
+      sum = order_item.quantity * order_item.product.price
+      @total_sum += sum
+    end
     respond_to do |format|
-      format.js { render json: @orders }
+      format.json #{ render json: @orders }
     end
   end
 
--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -5,18 +5,20 @@ const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
 const modal = document.getElementById('modal-btn');
-const something = document.getElementById('');
+const something = document.getElementById('tbody');
 
 modal.addEventListener('click', (event) => {
   console.log("It works!");
   something.innerHTML = '';
   fetch('/orders')
     .then(response => response.json())
-    .then((order_items) => {
-      console.log(order_items);
-      order_items.forEach((order_item) => {
-        const product = `<li>${order_item}</li>`;
+    .then((data) => {
+      console.log(data);
+      data.orders.forEach((order_item) => {
+        const product = `<tr> <th scope="row">${order_item.quantity}</th> <td>${order_item.product}</td> <td>${order_item.price}</td> <td><a><i class="fas fa-times"></i></a></td> </tr>`;
         something.insertAdjacentHTML("afterbegin", product);
       });
+      const total = `<tr class="total"> <th scope="row"></th> <td><strong>Total</strong></td> <td>R$ ${data.total_sum}</td> <td></td> </tr>`
+      something.insertAdjacentHTML('beforeend', total);
     });
 })
