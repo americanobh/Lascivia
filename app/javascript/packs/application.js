d515b1f49d4bea349b65b40a0ea6cc94357de90c
Felipe Americano <felip.americano@gmail.com>
2020-06-09 20:20:40 -0300

    apontei todos os scripts separados para a pasta script

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,17 @@ require("channels")
 require("./searchbar")
 
 import "bootstrap";
+import "jquery";
+import "popper";
+import "perfect-scrollbar";
+import "nouislider";
+import "chartjs";
+import "moment";
+import "bootstrap-datetimepicker";
+import "demo";
+import "blk-design-system-pro";
+
+
 
 // document.addEventListener('turbolinks:load', () => {
 // });
